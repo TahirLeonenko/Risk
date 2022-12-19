33d7692a5af4255bc027c3eb8fe751ac1c51048f
TahirLeonenko <tahir.leonenko@gmail.com>
2022-12-19 02:07:56 -0500

    Finished creating Board and Territory inner class (hopefully). Decided to change Territory from being an independent class to an inner class.

--- src/game_entities/BoardGraph.java
+++ src/game_entities/BoardGraph.java
@@ -2,16 +2,28 @@ package game_entities;
 
 import java.util.ArrayList;
 
-public interface BoardGraph<Territory> {
+public interface BoardGraph{
     /*
      * This gives the list of territories that are neighbouring the player's territory that aren't owned by the player
      * (territories that can be attacked)
      */
-    public ArrayList<Territory> getEnemyNeigbours(Territory territory);
+
+    public void addTerritory(String territoryName);
+
+    public void addEdge(String territoryName1, String territoryName2);
+
+    public ArrayList<String> getEnemyNeighbours(String territoryName);
 
     /*
      * This is the complete list of allied territories that has a path from the given territory.
      */
-    public ArrayList<Territory> getAlliedNeighbours(Territory territory);
+    public ArrayList<String> getAlliedNeighbours(String territoryName);
+
+    public void setOwner(String territoryName,String player);
+
+    public void setTroops(String territoryName, int troops);
+    public String getOwner(String territoryName);
+
+    public int getTroops(String territoryName, int troops);
 
 }
